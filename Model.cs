e1b4ca6540b9dbfca45387de4d004744334b8096
dkdrozdov <dkdrozdov@gmail.com>
2023-10-30 12:00:31 +0700

    init

---
+++ Model.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace C_2
+{
+    public class Model
+    {
+        public static class Encoder
+        {
+            public static string Encode(string text, IEnumerable<Tuple<string, string>> symbolToCode)
+            {
+                StringBuilder result = new();
+                char[] delimiters = { ',', ' ' };
+                var symbolSequence = text.Where((x) => !delimiters.Contains(x)).Select((x) => $"{x}");
+                foreach (var symbol in symbolSequence)
+                {
+                    var symbolEntry = symbolToCode.Where((x) => x.Item1 == symbol);
+                    if (!symbolEntry.Any())
+                        throw new ArgumentException(nameof(text),
+                            "Последовательность содержит отсутствующие в алфавите символы.");
+                    result.Append(symbolEntry.First().Item2 + " ");
+                }
+                return result.ToString();
+            }
+        };
+        public static class Decoder
+        {
+            public static string Decode(string text, IEnumerable<Tuple<string, string>> symbolToCode)
+            {
+                StringBuilder result = new();
+                char[] delimiters = { ',', ' ' };
+                var codeSequence = text.Split(delimiters).Where((x) => x.Contains('1') || x.Contains('0'));
+                foreach (var code in codeSequence)
+                {
+                    var codeEntry = symbolToCode.Where((x) => x.Item2 == code);
+                    if (!codeEntry.Any())
+                        throw new ArgumentException(nameof(text),
+                            "Последовательность содержит отсутствующие в коде алфавита кодовые слова.");
+                    result.Append(codeEntry.First().Item1 + " ");
+                }
+                return result.ToString();
+            }
+        };
+
+        public static bool IsKraft(IEnumerable<string> codewords)
+        {
+            var v = codewords.Select((x) => x.Length);
+            return v.Sum((l) => Math.Pow(2, -l)) <= 1;
+        }
+        public static double AverageCodeLength(IEnumerable<string> codewords)
+        {
+            var v = codewords.Select((x) => x.Length);
+            return v.Sum((l) => l) / (double)v.Count();
+        }
+        public static double CalculateRedundancy(IEnumerable<decimal> probabilities, double avgCodeLength)
+        {
+            double entropy = 0;
+            foreach (var p in probabilities)
+                entropy -= (double)p * Math.Log2((double)p);
+            return avgCodeLength - entropy;
+        }
+
+        public static List<Tuple<string, decimal, string>> CalculateWordsShannonFano(IEnumerable<Tuple<string, decimal>> symbolsProbabilities)
+        {
+            List<Tuple<string, decimal, string>> result = new();
+            Queue<IEnumerable<Tuple<string, decimal, string>>> queue = new();
+
+            // 0. Добавляем в очередь упорядоченный алфавит целиком
+            queue.Enqueue(symbolsProbabilities.Select((x) =>
+                Tuple.Create(x.Item1, x.Item2, "")).OrderByDescending((l) => l.Item2));
+
+            while (queue.Any())
+            {
+                // 1. Выбираем группу символов
+                var group = queue.Dequeue();
+                // 1.1 Группу, в которой остался только один символ добавляем в результат
+                if (group.Count() == 1)
+                    result.Add(group.First());
+                else
+                {
+                    decimal p = 0;
+                    Queue<Tuple<string, decimal, string>> q = new(group);
+                    List<Tuple<string, decimal, string>> group0, group1 = new();
+                    // 2. Разбиваем на 2 примерно равные по вероятности группы
+                    do
+                    {
+                        var e = q.Dequeue();
+                        p += e.Item2;
+                        // 3.а) К кодовым словам первой группы конкатенируем 1
+                        group1.Add(Tuple.Create(e.Item1, e.Item2, e.Item3 + "1"));
+                    }
+                    while (p < .5m * group.Sum((x) => x.Item2));
+                    // 3.б) К кодовым словам второй группы конкатенируем 0
+                    group0 = q.Select((e) => Tuple.Create(e.Item1, e.Item2, e.Item3 + "0")).ToList();
+                    // 4. Обе группы добавляем в очередь
+                    queue.Enqueue(group1);
+                    queue.Enqueue(group0);
+                }
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<Tuple<string, decimal, string>> GetWords(string alphabetFilePath, string probabilitiesFilePath)
+        {
+            char[] delimiters = { ',', ' ' };
+            var eps = 0.001m;
+            var alphabet = File.ReadAllText(alphabetFilePath).Split(delimiters);
+            var probabilities = (File.ReadAllText(probabilitiesFilePath).Split(delimiters).
+                Select((s) =>
+                {
+                    var i = Convert.ToDecimal(s);
+                    if (i <= 0 || i > 1) throw new ArgumentOutOfRangeException(nameof(probabilitiesFilePath),
+                        "Все вероятности должны быть больше 0 и не больше 1.");
+                    return i;
+                }));
+            if (!(probabilities.Sum((x) => x) - 1 < eps)) throw new ArgumentOutOfRangeException(nameof(probabilitiesFilePath),
+                        "Сумма вероятностей должна быть равна 1.");
+            if (alphabet.Length != probabilities.Count()) throw new ArgumentOutOfRangeException(nameof(probabilitiesFilePath),
+                         "Количество символов в алфавите и вероятностей не совпадает.");
+            if (!alphabet.Any() || !probabilities.Any()) throw new ArgumentOutOfRangeException(nameof(probabilitiesFilePath),
+                          "Алфавит пуст.");
+
+            return new List<Tuple<string, decimal, string>>(CalculateWordsShannonFano(
+                alphabet.Zip(probabilities, (symbol, probability) => Tuple.Create(symbol, probability))));
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,275 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace C_2
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public Model Model { get; init; }
+        public ObservableCollection<Tuple<string, decimal, string>> Words { get; set; } = new();
+        private string? probabilitiesFilePath;
+        private string? alphabetFilePath;
+        private string? textFilePath;
+        private string? resultFilePath;
+        public string? ProbabilitiesFilePath
+        {
+            get => probabilitiesFilePath;
+            set
+            {
+                probabilitiesFilePath = value;
+                OnPropertyChanged();
+                OnPathChanged();
+            }
+        }
+        public string? AlphabetFilePath
+        {
+            get => alphabetFilePath;
+            set
+            {
+                alphabetFilePath = value;
+                OnPropertyChanged();
+                OnPathChanged();
+            }
+        }
+        public string? TextFilePath
+        {
+            get => textFilePath;
+            set
+            {
+                textFilePath = value;
+                OnPropertyChanged();
+                OnPathChanged();
+            }
+        }
+        public string? ResultFilePath
+        {
+            get => resultFilePath;
+            set
+            {
+                resultFilePath = value;
+                OnPropertyChanged();
+                OnPathChanged();
+            }
+        }
+        private string? isKraft;
+        public string? IsKraft
+        {
+            get => isKraft;
+            set
+            {
+                isKraft = value;
+                OnPropertyChanged();
+            }
+        }
+        private string? averageCodeLength;
+        public string? AverageCodeLength
+        {
+            get => averageCodeLength;
+            set
+            {
+                averageCodeLength = value;
+                OnPropertyChanged();
+            }
+        }
+        private string? redundancy;
+        public string? Redundancy
+        {
+            get => redundancy;
+            set
+            {
+                redundancy = value;
+                OnPropertyChanged();
+            }
+        }
+        public ViewModel()
+        {
+            Model = new();
+            PathChanged += UpdateData;
+        }
+
+        private void UpdateData(object? sender, PropertyChangedEventArgs e)
+        {
+            if (AlphabetFilePath != null && ProbabilitiesFilePath != null &&
+                File.Exists(AlphabetFilePath) && File.Exists(ProbabilitiesFilePath))
+                try
+                {
+                    Words.Clear();
+                    var words = Model.GetWords(AlphabetFilePath, ProbabilitiesFilePath);
+                    foreach (var word in words)
+                        Words.Add(word);
+                    var codewords = Words.Select((x) => x.Item3);
+                    var _averageCodeLength = Model.AverageCodeLength(codewords);
+                    AverageCodeLength = $"{_averageCodeLength:F4}";
+                    var _redundancy = Model.CalculateRedundancy(Words.Select((x) => x.Item2), _averageCodeLength);
+                    Redundancy = $"{_redundancy:F4}";
+                    IsKraft = Model.IsKraft(codewords) ? "выполняется" : "не выполняется";
+                }
+                catch (Exception exception) when (exception is ArgumentOutOfRangeException)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+        }
+        private RelayCommand? encodeCommand;
+
+        public RelayCommand EncodeCommand
+        {
+            get
+            {
+                return encodeCommand ??
+                    (encodeCommand = new RelayCommand(obj =>
+                    {
+                        try
+                        {
+
+                            var s = Model.Encoder.Encode(File.ReadAllText(TextFilePath!),
+                                Words.ToList().Select((x) => Tuple.Create(x.Item1, x.Item3)));
+                            File.WriteAllText(ResultFilePath!, s);
+                            MessageBox.Show("Операция прошла успешно.");
+                        }
+                        catch (Exception e) when (e is ArgumentException)
+                        {
+                            MessageBox.Show(e.Message);
+                        }
+                    },
+                    (obj) =>
+                    AlphabetFilePath != null &&
+                    ProbabilitiesFilePath != null &&
+                    TextFilePath != null &&
+                    ResultFilePath != null &&
+                    File.Exists(AlphabetFilePath) &&
+                    File.Exists(ProbabilitiesFilePath) &&
+                    File.Exists(TextFilePath)));
+            }
+        }
+        private RelayCommand? decodeCommand;
+
+        public RelayCommand DecodeCommand
+        {
+            get
+            {
+                return decodeCommand ??
+                    (decodeCommand = new RelayCommand(obj =>
+                    {
+                        try
+                        {
+                            var s = Model.Decoder.Decode(File.ReadAllText(TextFilePath!),
+                                Words.ToList().Select((x) => Tuple.Create(x.Item1, x.Item3)));
+                            File.WriteAllText(ResultFilePath!, s);
+                            MessageBox.Show("Операция прошла успешно.");
+                        }
+                        catch (Exception e) when (e is ArgumentException)
+                        {
+                            MessageBox.Show(e.Message);
+                        }
+                    },
+                    (obj) =>
+                    AlphabetFilePath != null &&
+                    ProbabilitiesFilePath != null &&
+                    TextFilePath != null &&
+                    ResultFilePath != null &&
+                    File.Exists(AlphabetFilePath) &&
+                    File.Exists(ProbabilitiesFilePath) &&
+                    File.Exists(TextFilePath)));
+            }
+        }
+        private RelayCommand? exitCommand;
+        public RelayCommand ExitCommand
+        {
+            get
+            {
+                return exitCommand ??
+                    (exitCommand = new RelayCommand(obj =>
+                    {
+                        ((Window)obj).Close();
+                    }));
+            }
+        }
+        private RelayCommand? pickProbabilityPathCommand;
+        public RelayCommand PickProbabilityPathCommand
+        {
+            get
+            {
+                return pickProbabilityPathCommand ??
+                    (pickProbabilityPathCommand = new RelayCommand(obj =>
+                    {
+                        OpenFileDialog openFileDialog = new();
+                        if (openFileDialog.ShowDialog() == true)
+                        {
+                            ProbabilitiesFilePath = openFileDialog.FileName;
+                        }
+                    }));
+            }
+        }
+        private RelayCommand? pickAlphabetPathCommand;
+        public RelayCommand PickAlphabetPathCommand
+        {
+            get
+            {
+                return pickAlphabetPathCommand ??
+                    (pickAlphabetPathCommand = new RelayCommand(obj =>
+                    {
+                        OpenFileDialog openFileDialog = new();
+                        if (openFileDialog.ShowDialog() == true)
+                        {
+                            AlphabetFilePath = openFileDialog.FileName;
+                        }
+                    }));
+            }
+        }
+        private RelayCommand? pickTextPathCommand;
+        public RelayCommand PickTextPathCommand
+        {
+            get
+            {
+                return pickTextPathCommand ??
+                    (pickTextPathCommand = new RelayCommand(obj =>
+                    {
+                        OpenFileDialog openFileDialog = new();
+                        if (openFileDialog.ShowDialog() == true)
+                        {
+                            TextFilePath = openFileDialog.FileName;
+                        }
+                    }));
+            }
+        }
+        private RelayCommand? pickResultPathCommand;
+
+        public RelayCommand PickResultPathCommand
+        {
+            get
+            {
+                return pickResultPathCommand ??
+                    (pickResultPathCommand = new RelayCommand(obj =>
+                    {
+                        OpenFileDialog openFileDialog = new();
+                        if (openFileDialog.ShowDialog() == true)
+                        {
+                            ResultFilePath = openFileDialog.FileName;
+                        }
+                    }));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        public event PropertyChangedEventHandler? PathChanged;
+        public void OnPathChanged([CallerMemberName] string prop = "")
+        {
+            PathChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
